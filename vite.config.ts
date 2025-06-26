d71e50cdc2150d06b9515b636a093d74e6a7bb24
Willian C. Nunes <69167321+troopsofarchangel@users.noreply.github.com>
2025-06-26 17:06:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/badgesman/', // Caminho base para GitHub Pages
+  base: '/badgers/', // Caminho base para GitHub Pages
 });
